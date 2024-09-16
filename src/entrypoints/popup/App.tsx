26790f49a967b44d5a09fdfe3a3de85987bb86a3
ches <chestnut31415@gmail.com>
2024-09-17 05:59:58 +0900

    change title

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -3,7 +3,7 @@ import { Meta } from "@/components/Meta";
 export default function App() {
 	return (
 		<div className="m-8 font-sans text-base w-[500px]">
-			<h1 className="text-xl font-semibold">Meta Inspector</h1>
+			<h1 className="text-xl font-semibold">Meta Viewer</h1>
 			<Meta className="mt-6" />
 		</div>
 	);
